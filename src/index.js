8aba9289a086c067c6ea193f592c89589ae2c183
Bogus Franczak <bfranczak@gmail.com>
2019-05-05 19:53:57 +0200

    Complete task 28_3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import store from './store/store';
+import DevTools from './DevTools';
+import {getCountries} from './actions/countries';
 ReactDOM.render(
-  <h1>Initialize project</h1>,
-    document.getElementById('root'));
-    
+  <Provider store={store}>
+    <div>
+      <h1>Initialize project</h1>
+      <DevTools />
+    </div>
+  </Provider>,
+  document.getElementById('root'));
+
+  store.dispatch(getCountries());
