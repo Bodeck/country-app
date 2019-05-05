2c6f480a680a6e0d5be1cc9ff93c944ff23854d4
Bogus Franczak <bfranczak@gmail.com>
2019-05-05 18:20:58 +0200

    Create and export actions

---
+++ src/actions/countries.js
@@ -0,0 +1,39 @@
+export const GET_COUNTRIES = 'GET_COUNTRIES';
+export const GET_COUNTRY = 'GET_COUNTRY';
+export const DELETE_COUNTRY = 'DELETE_COUNTRY';
+export const SEARCH_COUNTRY = 'SEARCH_COUNTRY';
+export const SET_CONTINENT = 'SET CONTINENT';
+
+export function getCountries() {
+  return {
+    type: GET_COUNTRIES
+  }
+};
+
+export function getCountry(id) {
+  return {
+    type: GET_COUNTRY,
+    id
+  }
+};
+
+export function deleteCountry(id) {
+  return {
+    type: DELETE_COUNTRY,
+    id
+  }
+};
+
+export function searchCountry(searchText) {
+  return {
+    type: SEARCH_COUNTRY,
+    searchCountry
+  }
+};
+
+export function setContinent(name) {
+  return {
+    type: SET_CONTINENT,
+    name
+  }
+};
