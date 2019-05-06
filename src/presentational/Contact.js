8401b443b0160ea5d2b6665bc95b87e5dc1d9bfc
Bogus Franczak <bfranczak@gmail.com>
2019-05-07 00:29:30 +0200

    Complete routing implementation

---
+++ src/presentational/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => (
+  <h1>Ulica: Jakas tam</h1>
+);
+
+export default Contact;
---
+++ src/presentational/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => (
+  <h1>Strona domowa aplikacji państwa</h1>
+);
+
+export default Home;
