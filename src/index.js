3bdaedf81b3132fe7834f3e2fc60367e29877faa
Bogus Franczak <bfranczak@gmail.com>
2019-05-08 22:39:41 +0200

    Add country details functionality

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import store from './store/store';
 // import DevTools from './DevTools';
 import { Router, hashHistory } from 'react-router';
 import routes from './routes';
+import './country.css';
 
 ReactDOM.render(
   <Provider store={store}>
